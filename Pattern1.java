64c1232c7b7beaf526b803ac13ee85f8fe20dae3
Abhimn135 <127326272+Abhimn135@users.noreply.github.com>
2023-03-30 03:04:51 -0700

    Add files via upload

---
+++ Pattern1.java
@@ -0,0 +1,8 @@
+public class Pattern1{
+public static void main(String [] args){
+// printing 1 to 5 in pattern//
+
+for(int i=1; i<=5;i++){
+	for(int j=1;j<=i;j++){
+	System.out.print(j+" ");}
+	System.out.println("");}}}
---
+++ Pattern2.java
@@ -0,0 +1,8 @@
+public class Pattern2{
+public static void main(String [] args){
+// printing a to e in pattern//
+ int cha=65;
+for(int i=1; i<5;i++){
+	for(int j=0;j<i;j++){
+	System.out.print((char(cha+j))+" ");}
+	System.out.println("");}}}
